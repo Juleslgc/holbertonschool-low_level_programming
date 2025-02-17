4ef799ec6a63c02d9cc09e94a14f8d1f49ad78e2
Juleslgc <venturajules99@gmail.com>
2025-02-17 13:40:14 +0000

    Add a description found for return

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main for print "Programming is like building a multilingual puzzle".
- * return 0 in end.
+ * Return: return 0 in end.
  */
 int main(void)
 { puts("\"Programming is like building a multilingual puzzle");
