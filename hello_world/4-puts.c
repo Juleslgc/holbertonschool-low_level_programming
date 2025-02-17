d2ff1ee1694840a816420f915a887f847b3c231f
Juleslgc <venturajules99@gmail.com>
2025-02-17 13:24:46 +0000

    Add a \" in the text

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main for print "Programming is like building a multilingual puzzle".
+ * return 0 in end.
+ */
 int main ()
-{ puts ("\"Programming is like building a multilingual puzzle\n");
+{ puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
