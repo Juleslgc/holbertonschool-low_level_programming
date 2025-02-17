f6928b4725daab8a7aa5f804d3286aa2630e6fb1
Juleslgc <venturajules99@gmail.com>
2025-02-17 13:35:24 +0000

    Remove the space

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main for print "Programming is like building a multilingual puzzle".
  * return 0 in end.
  */
-int main (void)
+int main(void)
 { puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
