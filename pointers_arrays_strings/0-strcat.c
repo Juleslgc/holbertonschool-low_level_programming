62521164e7f1d5053c3c12760b71ec38faf770b3
Juleslgc <venturajules99@gmail.com>
2025-03-01 14:42:52 +0000

    Remove trailing whitespace in the code

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -12,9 +12,9 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int j;
 
-	for (j = 0 ; dest[j] != '\0' ; j++);
-
-	for (i = 0 ; src[i] != '\0' ;i++, j++)
+	for (j = 0 ; dest[j] != '\0' ; j++)
+		;
+	for (i = 0 ; src[i] != '\0' ; i++, j++)
 	{
 		dest[j] = src[i];
 	}
