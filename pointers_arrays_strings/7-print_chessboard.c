b3b7769bb00af8829ef3eb8d6a5108eb53fca575
Juleslgc <venturajules99@gmail.com>
2025-03-03 19:06:27 +0000

    Remove trailing whitespace for betty

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_chessboard - Function that prints the chessboard.
- * @a: It's a pointer
- * Return: return void in end.
- */
+* print_chessboard - Function that prints the chessboard.
+* @a: It's a pointer
+* Return: return void in end.
+*/
 
- void print_chessboard(char (*a)[8])
- {
-    int l;
-    int c;
+void print_chessboard(char (*a)[8])
+{
+	int l;
+	int c;
 
-    for (l = 0 ; l < 8 ; l++)
-    {
-        for (c = 0 ; c < 8 ; c++)
-        {
-            _putchar(a[l][c]);
-        }
-        _putchar('\n');
-    }
- }
+	for (l = 0 ; l < 8 ; l++)
+	{
+		for (c = 0 ; c < 8 ; c++)
+		{
+			_putchar(a[l][c]);
+		}
+		_putchar('\n');
+	}
+}
