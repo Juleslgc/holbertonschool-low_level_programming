7712d68af1cd736cc6786ba843be267c7832200c
Juleslgc <venturajules99@gmail.com>
2025-03-03 14:42:53 +0000

    Change the code

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -12,14 +12,18 @@ char *_strstr(char *haystack, char *needle)
 	int i;
 	int j;
 
-	for (i = 0 ; haystack[i] != '\0' ; i++)
+	for (i = 0 ; haystack[i] != '\0' ; haystack++)
 	{
-		for (j = 0; needle[j] != '\0' ; haystack++)
+		for (j = 0; needle[j] != '\0' ; j++)
 		{
-			if (haystack[i] == needle[j])
+			if (haystack[i + j] != needle[j])
 			{
-				return (haystack + i);
+				break;
 			}
+		}
+		if (needle[j] == 0)
+		{
+			return (haystack);
 		}
 	}
 	return ('\0');
