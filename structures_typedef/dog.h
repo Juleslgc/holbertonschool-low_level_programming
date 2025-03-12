7d6c3d0431c3119460e7cd447fdf65c07164ba23
Juleslgc <venturajules99@gmail.com>
2025-03-12 10:18:34 +0100

    Create the first structure in C

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef MY_DOG_H
+#define MY_DOG_H
+#include <stdio.h>
+/**
+ * struct dog - Dog
+ * @name: Name dog
+ * @age: Age dog
+ * @owner: Owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
