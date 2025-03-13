828115c1c990e403901c431928e3dc16e7e43c24
Juleslgc <venturajules99@gmail.com>
2025-03-13 10:01:10 +0100

    function that concatenates all the arguments of your program.

---
+++ malloc_free/100-argstostr.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * argstostr - Function that concatenates all the arguments of your program.
+ * @ac: It's a integer.
+ * @av: It's a character.
+ * Return: return character in end.
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, j;
+	char *a;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0 ; i < ac ; i++)
+	{
+		for(j = 0 ; av[i][j] != '\0' ; j++)
+		;
+		a = malloc(ac * sizeof(char));
+		if (a == NULL)
+		{
+			free(a);
+			return (NULL);
+		}
+		printf("%s\n", av[i]);
+	}
+	return (a);
+}
