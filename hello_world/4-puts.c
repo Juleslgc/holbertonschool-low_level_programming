27a5bde32bc7535b3f03142be219e46a48aa9a05
Juleslgc <venturajules99@gmail.com>
2025-03-18 12:10:50 +0100

    Retirer les fichiers ignorés du suivi

--- hello_world/4-puts.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-/**
- * main - for print "Programming is like building a multilingual puzzle".
- * Return: return 0 in end.
- */
-int main(void)
-{ puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
