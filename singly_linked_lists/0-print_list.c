f02db0070028274e43e248a214bf57443a017054
Juleslgc <venturajules99@gmail.com>
2025-03-31 12:28:06 +0200

    Feat: Add a function print list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - Function print all element of a list_t list.
+ * @h: It's a pointer that points to the first node.
+ * Return: return the number of node in the list..
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
