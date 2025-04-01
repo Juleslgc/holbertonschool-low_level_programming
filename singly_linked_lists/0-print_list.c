be076bb8f7a1e2085b3cc9547af0afba97b87fe7
Juleslgc <venturajules99@gmail.com>
2025-04-01 09:40:23 +0200

    feat: Add function add node at the end of a list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,7 +3,7 @@
 /**
  * print_list - Function print all element of a list_t list.
  * @h: It's a pointer that points to the first node.
- * Return: return the number of node in the list..
+ * Return: return the number of node in the list.
  */
 
 size_t print_list(const list_t *h)
--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
 /**
  * _strlen - Function that returns the length of a string.
  * @s: It's a char.
@@ -18,15 +17,15 @@ int _strlen(const char *s)
 }
 
 /**
- * add_node - Function that adds a new node at the beginning of a list_t list.
- * @head: It's a pointer that pointe the beginning the list.
+ * add_node_end - Function that adds a new node at the end of a list_t list.
+ * @head: It's a pointer that pointe the end the list.
  * @str: It's a pointer that pointe the new node.
  * Return: return new node in end.
  */
 
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
+	list_t *new_node, *temp;
 
 	if (str == NULL)
 	{
@@ -45,7 +44,16 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 	new_node->len = _strlen(str);
-	new_node->next = *head;
-	*head = new_node;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	temp = *head;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = new_node;
 	return (new_node);
 }
