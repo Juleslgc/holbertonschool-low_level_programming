7acfee9e621003f7c52123a518246ed7aeb4cc3c
Juleslgc <venturajules99@gmail.com>
2025-04-01 10:44:30 +0200

    feat: Add function that free list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * free_list - Function that free the list.
+ * @head: It's a pointer that pointe the beginning the list.
+ * Return: return void in end.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	while (head != NULL)
+	{
+		free(head->str);
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
