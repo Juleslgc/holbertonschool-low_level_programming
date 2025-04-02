b9850e3cf08a1537565f2240448af601094671e8
Juleslgc <venturajules99@gmail.com>
2025-04-02 14:31:31 +0200

    fix: take the index condition out of the loop

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -11,16 +11,17 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *temp = *head;
 	unsigned int i;
 
-	if (*head == NULL && index == 0)
+	if (*head == NULL)
 	{
 		return (-1);
 	}
+	if (index == 0)
+	{
+		*head = temp->next;
+		return (1);
+	}
 	for (i = 0 ; temp != NULL && i < index ; i++)
 	{
-		if (index == 0)
-		{
-			*head = temp;
-		}
 		temp = temp->next;
 	}
 	if (temp == NULL)
@@ -34,10 +35,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (temp->next != NULL)
 	{
 		temp->next->prev = temp->prev;
-	}
-	if (*head == temp)
-	{
-		*head = temp->next;
 	}
 
 	return (1);
