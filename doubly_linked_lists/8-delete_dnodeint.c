a122ce2e29259cd7d2aa1936d26f5558d2f5899f
Juleslgc <venturajules99@gmail.com>
2025-04-02 15:14:11 +0200

    fix: add condition in the condition  index egal zero

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -18,6 +18,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = temp->next;
+		if (*head != NULL)
+		{
+			(*head)->prev = NULL;
+		}
         free(temp);
 		return (1);
 	}
