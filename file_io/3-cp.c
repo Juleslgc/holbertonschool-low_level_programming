64b2514af316c8f465faeeffe187b422371806d4
Juleslgc <venturajules99@gmail.com>
2025-04-04 15:13:15 +0200

    fix: Changed flag on the first file descriptor

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 	{	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fdf = open(argv[1], O_RDWR);
+	fdf = open(argv[1], O_RDONLY);
 	if (fdf == -1)
 	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
